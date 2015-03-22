be47c7fb57ab97aa8ec585ebbd496c0d7880c4ba
TingYang Lin <tonylin57@gmail.com>
2015-03-22 13:27:48 -0700

    Added ping

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-var mraa = require("mraa");
+var deviceInterface = require('./lib/device-interface');
 var WebSocketServer = require('ws').Server;
-var uuid = require('uuid');
-var myAnalogPin = new mraa.Aio(0);
+var os = require('os');
+var ifaces = os.networkInterfaces();
 
 function pollPin(cb) {
     'use strict';
+    var lastStatus=0;
     setInterval(function () {
-        var a = myAnalogPin.read();
-        cb(null, {reading:a});
-    }, 4000);
+        deviceInterface.status(function(err,reading){
+            if(reading!= lastStatus){
+                // status has been updated
+                cb(err,{reading:reading});
+            }
+            lastStatus = reading;
+        });
+    }, 500);
 }
 
-function listen(){
+function handleRequest(payload, client){
+    var request = JSON.parse(payload);
+    switch(request.command){
+        case 'on':
+            deviceInterface.on();
+            break;
+        case 'off':
+            deviceInterface.off();
+            break;
+        case 'status':
+        default :
+            deviceInterface.status(function(err, status){
+                var payload = JSON.stringify(status);
+                client.send(payload);
+            });
+            break;
+    }
+}
+
+function listen(cb){
     var wss = new WebSocketServer({ port: 80 }),
         clients = {};
 
@@ -20,8 +45,8 @@ function listen(){
         var connectionId = uuid.v4();
         clients[connectionId] = ws;
 
-        ws.on('message', function incoming(message) {
-            console.log('Message from board: %s', message);
+        ws.on('message', function(msg){
+            handleRequest(msg, ws);
         });
 
         ws.on('close', function close() {
@@ -38,6 +63,33 @@ function listen(){
             ws.send(payload);
         });
     })
+    cb();
 }
 
-listen();
+listen(function(){
+
+    var networkInterfaces = [];
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                networkInterfaces.push(iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                networkInterfaces.push(iface.address);
+            }
+        });
+    });
+
+    request({url:'http://api.swych.com/device/register',qs:{ip:networkInterfaces.split(',')}});
+    setInterval(function () {
+        request({url:'http://api.swych.com/device/ping',qs:{ip:networkInterfaces.split(',')}});
+    }, 500);
+});
